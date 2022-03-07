c5dde113cf8860d65959ad8506a7053b234dce77
jinjoo <pearls0803@naver.com>
2022-03-07 09:55:56 +0900

    member table id -> userid 로 변경

--- SpringWeb03_ShoppingMall/src/main/webapp/WEB-INF/views/sql.sql
+++ SpringWeb03_ShoppingMall/src/main/webapp/WEB-INF/views/sql.sql
@@ -4,7 +4,7 @@ DROP TABLE worker CASCADE CONSTRAINTS;
 
 -- member 테이블 : 상품을 구매하기위해서 회원으로 가입한 구매자들.
 CREATE TABLE member (
-	id varchar2(20) NOT NULL ,
+	userid varchar2(20) NOT NULL ,
 	pwd varchar2(20) NOT NULL,
 	name varchar2(20) NOT NULL,
 	email varchar2(40) NOT NULL,
@@ -13,7 +13,7 @@ CREATE TABLE member (
 	phone varchar2(20) NOT NULL,
 	useyn char(1) DEFAULT 'y',    -- 휴면 계정 여부
 	indate date DEFAULT sysdate,
-	PRIMARY KEY (id)
+	PRIMARY KEY (userid)
 );
 
 select * from member;
@@ -29,9 +29,9 @@ CREATE TABLE product(
 	price3 number(7),   -- 마진 
 	content varchar2(1000),
 	image varchar2(50),
-	useyn char(1) DEFAULT 'y',     -- 상품 판매 유효 여부
+	--useyn char(1) DEFAULT 'y',     -- 상품 판매 유효 여부
 	bestyn char(1) DEFAULT 'n',   -- 베스트상품 진열 여부
-	indate date DEFAULT sysdate,    -- 등록일
+	--indate date DEFAULT sysdate,    -- 등록일
 	PRIMARY KEY (pseq)
 );
 
@@ -158,9 +158,9 @@ insert into worker values('scott', 'tiger', '홍길동', '010-6400-6068');
 
 
 -- 회원 입력
-insert into member(id, pwd, name, zip_num, address, phone, email) values
+insert into member(userid, pwd, name, zip_num, address, phone, email) values
 ('one', '1111', '김나리', '133-110', '서울시 성동구 성수동1가 1번지21호', '017-777-7777','acc@abc.com');
-insert into member(id, pwd, name, zip_num, address, phone, email)values
+insert into member(userid, pwd, name, zip_num, address, phone, email)values
 ('two', '2222', '김길동', '130-120', '서울시 송파구 잠실2동 리센츠 아파트 201동 505호', '011-123-4567','acc@abc.com');
 
 
